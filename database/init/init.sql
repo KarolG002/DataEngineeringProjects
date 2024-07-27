4a147ce09746ba3b62c72f0dc35de6bef95a7657
DESKTOP-197DBPJ <karolgawlakanalityka@gmail.com>
2024-07-27 13:36:03 +0200

    Added inserts for postgres docker db

--- database/init/init.sql
+++ database/init/init.sql
@@ -1,12 +1,17 @@
-CREATE TABLE movies (
+
+--CREATE DATABASE moviesdb;
+
+-- \c movies_db;
+
+CREATE TABLE IF NOT EXISTS movies (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     release_year INT NOT NULL,
     rated VARCHAR(255) NOT NULL,
-    runtime VARCHAR(255) NOT NULL,
+    runtime INT NOT NULL,
     genre VARCHAR(255) NOT NULL,
     country VARCHAR(255) NOT NULL,
     awards VARCHAR(255) NOT NULL,
-    ratings_imdb DECIMAL(3,1) NOT NULL,
+    ratings_imdb DECIMAL(3,1) NOT NULL
 );
       
--- src/pipeline/movies.py
+++ src/pipeline/movies.py
@@ -5,12 +5,12 @@ import datetime
 import os
 import json
 from prefect import flow, get_run_logger, task
-
+import psycopg2
 #CONSTANTS
 API_URL = "http://www.omdbapi.com/"
 RECORDS_PER_DAY = 5
 STATE_FILE = "pagination_state.json"
-
+#new
 @task
 def load_state():
     if os.path.exists(STATE_FILE):
@@ -54,24 +54,73 @@ def clean_movie_data(movie_data_dict: dict) -> dict:
             imdb_rating = rating.get("Value")
             imdb_rating = imdb_rating.split('/')[0]
             break
-    runtime_raw = movie_data_dict.get("Runtime", 0)
+    runtime_raw = movie_data_dict.get("Runtime", "0")
     runtime_cleaned = None
     if runtime_raw:
         runtime_cleaned = runtime_raw.replace(" min", "").strip()
-
-    return {
-        "title": movie_data_dict.get("Title", 0),
-        "year": movie_data_dict.get("Year", 0),
-        "rated": movie_data_dict.get("Rated", 0),
-        "runtime": runtime_cleaned,
-        "genre": movie_data_dict.get("Genre", 0),
-        "country": movie_data_dict.get("Country", 0),
-        "awards": movie_data_dict.get("Awards", 0),
-        "ratings_imdb": imdb_rating,
+    
+    cleaned_data = {
+        "title": movie_data_dict.get("Title", "0"),
+        "release_year": int(movie_data_dict.get("Year", 0)),
+        "rated": movie_data_dict.get("Rated", "0"),
+        "runtime": int(runtime_cleaned),
+        "genre": movie_data_dict.get("Genre", "0"),
+        "country": movie_data_dict.get("Country", "0"),
+        "awards": movie_data_dict.get("Awards", "0"),
+        "ratings_imdb": float(imdb_rating) if imdb_rating else "0",
     }
 
+
+    print("Cleaned Data:", cleaned_data)
+    return cleaned_data
+@task
+def insert_to_db(
+    cleaned_data: dict,
+    db_host: str,
+    db_user: str,
+    db_pass: str,
+    db_name: str,
+):
+    query = """
+    INSERT INTO movies (
+        title,
+        release_year,
+        rated,
+        runtime,
+        genre,
+        country,
+        awards,
+        ratings_imdb
+    ) VALUES (
+        %(title)s,
+        %(release_year)s,
+        %(rated)s,
+        %(runtime)s,
+        %(genre)s,
+        %(country)s,
+        %(awards)s,
+        %(ratings_imdb)s
+    )
+    """
+    try:
+        with psycopg2.connect(
+            user=db_user, password=db_pass, dbname=db_name, host=db_host
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(query, cleaned_data)
+                connection.commit()
+    except psycopg2.Error as e:
+        print(f"Database error: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
 @flow
-def process_movies():
+def process_movies( 
+    db_host: str="localhost",
+    db_user: str="root",
+    db_pass: str="root",
+    db_name: str="moviesdb",
+):
     state = load_state()
     last_index = state["last_index"]
     last_date = state["last_date"]
@@ -92,13 +141,15 @@ def process_movies():
             print(f"Fetching data for: {movie_title} made in {movie_year}")
             movie_data_dict = retrieve_movie_from_api(movie_title, movie_year)
             cleaned_data = clean_movie_data(movie_data_dict)
-            print(cleaned_data)
+            insert_to_db(cleaned_data, db_host, db_user, db_pass, db_name,)
+            print(f"Inserted data for: {movie_title}")
         except requests.RequestException as e:
             print(f"Error fetching data for {movie_title}: {e}")
+        except Exception as e:
+            print(f"Error processing data for {movie_title}: {e}")
 
         last_index = i + 1
     save_state(last_index, today)
-
 
 def main():
     process_movies.serve(
